32f7f1db4e57310afc4d0861a4c96a3135ecf1af
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-09-25 14:31:14 +0100

    Created seperate protected pages for profiles and chat

---
+++ components/gatherly/Chat/Chat.jsx
@@ -0,0 +1,12 @@
+import styles from "./Chat.module.css";
+// import Image from "next/image";
+
+export default function Chat() {
+  return (
+    <>
+      <section>
+        <h1 className={styles.title}>Chat</h1>
+      </section>
+    </>
+  );
+}
