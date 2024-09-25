eed697faff75e76311639d3d9028dda849d46ff2
Paulina G <paulinag@Paulinas-MacBook-Pro.local>
2024-09-25 13:22:34 +0100

    css

--- components/gatherly/Profile/Profile.jsx
+++ components/gatherly/Profile/Profile.jsx
@@ -1,6 +1,37 @@
 import styles from "./Profile.module.css";
-
+// import { useState, useEffect } from "react";
 export default function Profile() {
+  // const [location, setLocation] = useState("Fetching location...");
+  // const [error, setError] = useState("");
+
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         const { latitude, longitude } = position.coords;
+  //         fetchLocationData(latitude, longitude);
+  //       },
+  //       (error) => {
+  //         setError("Unable to retrieve location.");
+  //       }
+  //     );
+  //   } else {
+  //     setError("Geolocation is not supported by this browser.");
+  //   }
+  // }, []);
+
+  // // Function to fetch readable address from coordinates using OpenStreetMap or other API
+  // const fetchLocationData = async (latitude, longitude) => {
+  //   try {
+  //     const response = await fetch(
+  //       `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+  //     );
+  //     const data = await response.json();
+  //     setLocation(data.display_name);
+  //   } catch (error) {
+  //     setError("Unable to retrieve location data.");
+  //   }
+  // };
+
   return (
     <>
       <section>
