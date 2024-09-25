181387d215eb072e43342777cb639506cf21b1a9
SuperFlyz81 <109296884+SuperFlyz81@users.noreply.github.com>
2024-09-25 13:57:12 +0100

    Static profile page created. Header and footer customized

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,6 @@ export default function RootLayout({
                 </p>
                 <ThemeSwitcher />
               </footer>
-              
             </div>
           </main>
         </ThemeProvider>
